4569c1581724a9c70343acb3e89d934501d5b4bc
Zahra_786 <ghulamzahra8816@gmail.com>
2025-12-30 18:50:55 +0500

    Import MySQL Py

--- backend/scholarstay/__init__.py
+++ backend/scholarstay/__init__.py
@@ -1,7 +1,5 @@
 import pymysql
 pymysql.install_as_MySQLdb()
-
-# Spoof version for Django compatibility
 import MySQLdb
 if not hasattr(MySQLdb, 'version_info') or MySQLdb.version_info < (2, 2, 7):
     MySQLdb.version_info = (2, 2, 7, 'final', 0)
