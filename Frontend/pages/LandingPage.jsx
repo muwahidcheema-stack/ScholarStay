b23636395558d27559f78c2bbe7b0cd5bc198dad
Zahra_786 <ghulamzahra8816@gmail.com>
2025-12-30 18:54:49 +0500

    Update LandingPage

--- Frontend/pages/LandingPage.jsx
+++ Frontend/pages/LandingPage.jsx
@@ -1,12 +1,6 @@
 import Navbar from '../components/Navbar.jsx';
-import Landing from './Landing.jsx'; // CORRECTED IMPORT
-
-export default function LandingPage() {
-    return (
-        <>
-            <Navbar />
-            <Landing />
-        </>
-
-    );
+import Landing from './Landing.jsx'; 
+export default function LandingPage() 
+{
+    return (<><Navbar /><Landing /></>);
 }
